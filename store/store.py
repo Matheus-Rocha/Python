ff818a20b12a8c4e4061b203fc05321a3dfe933f
Matheus Rocha <49811477+mthsrocha@users.noreply.github.com>
2021-12-05 18:41:21 -0300

    store POO

--- store/store.py
+++ store/store.py
@@ -3,13 +3,11 @@ from datetime import datetime, timedelta
 # Produtos:
 class Veiculo:
 
-    def __init__(self, preco, placa, cor, marca, n_rodas, velocidade_media):
-        self.preco = preco
-        self.placa = placa
-        self.cor = cor
-        self.marca = marca
-        self.numero_de_rodas = n_rodas
-        self.velocidade_media = velocidade_media
+    def __init__(self):
+        self.vendido = False
+
+    def tem_na_loja(self):
+        return False if self.vendido else True
 
     def valor_de_venda(self):
         return self.preco
@@ -19,127 +17,133 @@ class Veiculo:
         return self.preco - desvalorizacao
 
     def velocidade_media(self):
-        return self.velocidade_media
-
+        if self.potencia:
+            return self.velocidade_media * (self.potencia * 0.1)
+        return self.velocidade_media * (self.cilindrada * 0.1)
 
 
 class Carro(Veiculo):
 
-    def __init__(self, potencia, airbag):
+    def __init__(self, preco, placa, cor, marca, velocidade_media, potencia, airbag):
+        self.preco = preco
+        self.placa = placa
+        self.cor = cor
+        self.marca = marca
+        self.velocidade_media = velocidade_media
         self.potencia = potencia
         self.airbag = airbag
-
-    def velocidade_media(self):
-        return self.velocidade_media * (self.potencia * 0.05)
 
 
 class Caminhao(Veiculo):
 
-    def __init__(self, potencia):
+    def __init__(self, preco, placa, marca, velocidade_media, potencia, tamanho_do_bau):
+        self.preco = preco
+        self.placa = placa
+        self.marca = marca
+        self.velocidade_media = velocidade_media
         self.potencia = potencia
-        self.carga
-        self.tamanho_do_bau
-    
-    def velocidade_media(self):
-        pass
+        self.tamanho_do_bau = tamanho_do_bau
 
     def quantidade_max_carga(self):
-        pass
+        return self.tamanho_do_bau
 
 
 class Moto(Veiculo):
 
-    def __init__(self, cilindrada):
+    def __init__(self, preco, placa, cor, marca, velocidade_media, cilindrada):
+        self.preco = preco
+        self.placa = placa
+        self.cor = cor
+        self.marca = marca
+        self.velocidade_media = velocidade_media
         self.cilindrada = cilindrada
-    
-    def velocidade_media(self):
-        pass
 
 
 # Administradores:
 class Funcionario:
-    
-    def __init__(self):
-        self.funcionario_id
-        self.nome
-        self.dias_de_empresa
 
     def tempo_de_empresa(self):
         time = datetime.now() - timedelta(days=self.dias_de_empresa)
+        return time.isoformat()
 
 
 class Gerente(Funcionario):
-    
-    def __init__(self):
-        pass
 
-    def autorizar_desconto(self):
-        pass
+    def __init__(self, id, nome, dias_de_empresa):
+        self.funcionario_id = id
+        self.nome = nome
+        self.dias_de_empresa = dias_de_empresa    
 
-    def autorizar_compra(self):
-        pass
+    def autorizar_desconto(self, forma_de_pagamento, parcelamento):
+        if forma_de_pagamento in ['dinheiro', 'debito'] and parcelamento == 'a vista':
+            return True
+        
+    def autorizar_compra(self, veiculo, preco_venda):
+        preco_aceito_compra = veiculo.preco - (veiculo.preco * 0.2)
+        if preco_venda >= preco_aceito_compra:
+            return True 
 
 
 class Vendedor(Funcionario):
 
-    def __init__(self):
-        pass
+    def __init__(self, id, nome, dias_de_empresa):
+        self.funcionario_id = id
+        self.nome = nome
+        self.dias_de_empresa = dias_de_empresa 
 
-    def vender_veiculo(self):
-        pass
+    def vender_veiculo(self, cliente, veiculo):
+        if cliente.carteira >= veiculo.preco:
+            return True
 
-    def comprar_veiculo(self):
-        pass
+    def comprar_veiculo(self, cliente, veiculo, gerente):
+        if gerente.autorizar_compra:
+            cliente.carteira += veiculo.preco
+            return cliente.carteira
 
 
 class Caixa(Funcionario):
 
-    def __init__(self):
-        pass
+    def __init__(self, id, nome, dias_de_empresa):
+        self.funcionario_id = id
+        self.nome = nome
+        self.dias_de_empresa = dias_de_empresa 
+        self.valor_em_caixa = 0
 
-    def receber_pagamento(self):
-        pass
-
-    def dar_troco(self):
-        pass
-
+    def receber_pagamento(self, vendedor, cliente, veiculo):
+        if vendedor.vender_veiculo():
+            cliente.carteira -= veiculo.preco
+            self.valor_em_caixa += veiculo.preco
+            veiculo.vendido = True
+            return f'Veiculo: {veiculo.marca} {veiculo.cor} {veiculo.placa} vendido por {veiculo.preco} vendido!'
+    
+    def fazer_pagamento(self, vendedor, cliente, veiculo, preco):
+        if vendedor.comprar_veiculo():
+            self.valor_em_caixa -= preco
+            cliente.carteira += preco
+            veiculo.vendido = True
+            return f'Veiculo: {veiculo.marca} {veiculo.cor} {veiculo.placa} vendido por {veiculo.preco} vendido!'
+            
 
 class Cliente:
 
-    def __init__(self):
-        self.carteira
-        self.tipo
+    def comprar_veiculo(self, marca, cor, preco, veiculo):
+        preco = veiculo.preco - (veiculo.preco * self.porcentagem_desconto)
+        if veiculo.marca == marca and veiculo.cor == cor and veiculo.preco <= preco:
+            return True
 
-    def beneficios(self):
-        pass
-
-    def comprar_veiculo(self):
-        pass
-
-    def vender_veiculo(self):
-        pass
-
+    def vender_veiculo(self, veiculo, preco):
+        if veiculo.preco <= preco:
+            return True
 
 class ClienteGold(Cliente):
 
-    def __init__(self):
+    def __init__(self, carteira):
+        self.carteira = carteira
         self.porcentagem_desconto = 0.10
-
-    def beneficios_gold(self):
-        pass
 
 
 class ClienteVip(Cliente):
 
-    def __init__(self):
+    def __init__(self, carteira):
+        self.carteira = carteira
         self.porcentagem_desconto = 0.15
-
-    def beneficios_vip(self):
-        pass
-
-
-def venda_de_veiculo():
-    pass
-
-def compra_de_veiculo():
-    pass
